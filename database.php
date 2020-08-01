4fec2af47cf3c3f947dad4b1a67720459f883a82
ankitkataria7890 <58991431+ankitkataria7890@users.noreply.github.com>
2020-08-01 22:50:35 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-$host='localhost';
-$dbuser='root';
-$dbpassword='';
-$dbname='image';
+$host='sql12.freedatabase.com';
+$dbuser='sql12358092';
+$dbpassword='DHAXcZJdM2';
+$dbname='sql12358092';
 $con=mysqli_connect($host,$dbuser,$dbpassword,$dbname);
 if(!$con){
  die("not connected");}
