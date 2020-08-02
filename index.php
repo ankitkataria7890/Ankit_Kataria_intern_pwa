9ca569dad35e879b09d9b752caeee97fd3b9f380
ankitkataria7890 <58991431+ankitkataria7890@users.noreply.github.com>
2020-08-02 22:43:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -34,8 +34,11 @@ function load(id,url) {
 	xhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 		document.getElementById(id).innerHTML = this.responseText;
-		
+	else {
+		document.getElementById(id).innerHTML = "Loading...";
 		}
+		
+	}
 	else{
 		console.log(this.status);
 		}
