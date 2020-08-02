b8a8553705e98c9a85f29640aa0d418d65fc46da
ankitkataria7890 <58991431+ankitkataria7890@users.noreply.github.com>
2020-08-02 22:50:15 +0530

    Update main_content.php

--- main_content.php
+++ main_content.php
@@ -8,25 +8,25 @@ $content_r=mysqli_num_rows($content_q);
 while($row=mysqli_fetch_array($content_q)){
 	
 	echo '<div class="title"><p>'.ucwords($row['title']).'</p></div>';
-	echo '<div class="image" >';
+	echo '<div class="image" style="background-image:url('logo1.jpg')">';
 	if($row['img1']!=""){
-		echo "<img src='".$row['img1']."'>";
+		echo "<img src='".$row['img1']."' loading='lazy'>";
 		//echo'<a class="carousel-item" href="#one!"><img src="'.$row["img1"].'"></a>';
 	}
 	if($row['img2']!=""){
-		echo "<img src='".$row['img2']."'>";
+		echo "<img src='".$row['img2']."' loading='lazy'>";
 		//echo'<a class="carousel-item" href="#one!"><img src="'.$row["img2"].'"></a>';
 	}
 	if($row['img3']!=""){
-		echo "<img src='".$row['img3']."'>";
+		echo "<img src='".$row['img3']."' loading='lazy'>";
 		//echo'<a class="carousel-item" href="#one!"><img src="'.$row["img3"].'"></a>';
 	}
 	if($row['img4']!=""){
-		echo "<img src='".$row['img4']."'>";
+		echo "<img src='".$row['img4']."' loading='lazy'>";
 		//echo'<a class="carousel-item" href="#one!"><img src="'.$row["img4"].'"></a>';
 	}
 	if($row['img5']!=""){
-		echo "<img src='".$row['img5']."'>";
+		echo "<img src='".$row['img5']."' loading='lazy'>";
 		//echo'<a class="carousel-item" href="#one!"><img src="'.$row["img5"].'"></a>';
 	}
 	//echo '<a class="prev" onclick="plusSlides(this)">&#10094;</a>';
